8fd1891ae755a109e524b16c4607fd5e1dddc5d4
B.Lamine <medlamine.bellilet@outlook.com>
2024-07-20 11:53:14 -0700

    1-pack_web_static.py: create file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Fabfile for web site resources compression.
+"""
+
+
+import datetime
+from fabric.api import *
+
+
+def do_pack():
+    """ Local compression command.
+    """
+    update_date = datetime.datetime.now()
+    filename = "web_static_{}.tgz".format(update_date.strftime("%Y%m%d%H%M%S"))
+    local("mkdir -p versions")
+    with lcd("./versions"):
+        test = local("tar -cvzf {} ../web_static".format(filename),
+                      capture=True)
+        if test.return_code == 0:
+            return "/versions/" + filename
+        else:
+            return None
