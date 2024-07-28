6f38f9ea94da45afd35160bf21606d1cfb0aa50f
B.Lamine <medlamine.bellilet@outlook.com>
2024-07-28 10:54:53 -0700

    web_flask/5-number_template.py, templates/5-number.html: create files and folder

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Minimalist Flask web app
+"""
+
+
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Index web page response.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB response web page.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text=''):
+    """ C_is_fun response web page.
+    """
+    if '_' in text:
+        tmp_list = [c if c != '_' else ' ' for c in list(text)]
+        text = ''.join(tmp_list)
+    return "C {}".format(escape(text))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def pythonisfun(text='is cool'):
+    """ Python_is_fun response web page.
+    """
+    if '_' in text:
+        tmp_list = [c if c != '_' else ' ' for c in list(text)]
+        text = ''.join(tmp_list)
+    return "Python {}".format(escape(text))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def isnumber_template(n):
+    """  template HTML response web page only for numbers (int).
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
