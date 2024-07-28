84480e7ea42c15e90bf265b49520c48d100a1f59
B.Lamine <medlamine.bellilet@outlook.com>
2024-07-28 07:15:48 -0700

    web_flask/2-c_route.py: create file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Minimalist Flask web app
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Index web page response.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB response web page.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text=''):
+    """ C_is_fun response web page.
+    """
+    if '_' in text:
+        tmp_list = [c if c != '_' else ' ' for c in list(text)]
+        text = ''.join(tmp_list)
+    return "C {}".format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
